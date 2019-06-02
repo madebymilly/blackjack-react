8114d3bba70ad395eff0105ab187ba40e37a3666
Milly Uylenbroek <m.uylenbroek@eagerly.nl>
2019-06-02 16:24:21 +0200

    added icons for suits with react-icons

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react'
+import { GiClubs, GiSpades, GiHearts, GiDiamonds } from 'react-icons/gi'
 
-const Card = ({ suit, value }) => {
-  return (
-    <div className="card js-card">
-      <span>{value} of {suit}</span>
-    </div>
-  )
+class Card extends React.Component {
+
+  renderSwitch(param) {
+    switch(param) {
+      case 'hearts':
+        return <GiHearts />;
+
+      case 'spades':
+       return <GiSpades />;
+
+      case 'clubs':
+        return <GiClubs />;
+
+      case 'diamonds':
+        return <GiDiamonds />;
+
+      default:
+        return '';
+    }
+  }
+
+
+  render() {
+    const value = this.props.value;
+    const suit = this.props.suit;
+    return (
+      <div className="card js-card">
+        <span>{value} of {suit}</span>
+        {this.renderSwitch(suit)}
+      </div>
+    )
+  }
 }
 
 export default Card;
+
+//
+//
+//
